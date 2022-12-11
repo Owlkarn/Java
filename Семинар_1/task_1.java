5fe6b5016bf4529ebee45ec4914e8a20574f4587
Vedeneev Ilya <tegrozayadc@yandex.ru>
2022-12-11 21:11:54 +0500

    HW 1 done

--- Семинар_1/task_1.java
+++ Семинар_1/task_1.java
@@ -1,31 +1,3 @@
-// package Семинар_1;
-
-// public class task_1 {
-//     public static void main(String[] args) {
-//         int[] a = { 1, 9, 9, 0, 2, 8, 0, 9 };
-//         int count = 0;
-
-//         for (int i = 0; i < a.length; i++) {
-//             for (int j = 0; j < a.length; j++) {
-//                 if (a[i] == a[j] && j < i) break;
-//                 else if (a[i] == a[j] && j >= i) {
-//                     count++;
-//                 }
-//             }
-//             if (count != 0){
-//                 if (count > 1 && count < 5){
-//                     System.out.printf("%d встречается %d раза", a[i], count);
-//                 }
-//                 else {
-//                     System.out.printf("%d встречается %d раз", a[i], count);
-//                 }
-//                 System.out.println();
-//                 count = 0;
-//             }
-//         }
-//     }
-// }
-
 package Семинар_1;
 
 public class task_1 {
@@ -34,18 +6,17 @@ public class task_1 {
         int count = 0;
 
         for (int i = 0; i < a.length; i++) {
-            if (a[i] != null) {
-                for (int j = i; j < a.length; j++) {
-                    if (a[i] == a[j]) {
-                        count++;
-                        a[j] = null;
-                    }
+            for (int j = 0; j < a.length; j++) {
+                if (a[i] == a[j] && j < i) break;
+                else if (a[i] == a[j] && j >= i) {
+                    count++;
                 }
             }
-            if (count != 0) {
-                if (count > 1 && count < 5) {
+            if (count != 0){
+                if (count > 1 && count < 5){
                     System.out.printf("%d встречается %d раза", a[i], count);
-                } else {
+                }
+                else {
                     System.out.printf("%d встречается %d раз", a[i], count);
                 }
                 System.out.println();
@@ -54,3 +25,32 @@ public class task_1 {
         }
     }
 }
+
+// package Семинар_1;
+
+// public class task_1 {
+//     public static void main(String[] args) {
+//         int[] a = { 1, 9, 9, 0, 2, 8, 0, 9 };
+//         int count = 0;
+
+//         for (int i = 0; i < a.length; i++) {
+//             if (a[i] != null) {
+//                 for (int j = i; j < a.length; j++) {
+//                     if (a[i] == a[j]) {
+//                         count++;
+//                         a[j] = null;
+//                     }
+//                 }
+//             }
+//             if (count != 0) {
+//                 if (count > 1 && count < 5) {
+//                     System.out.printf("%d встречается %d раза", a[i], count);
+//                 } else {
+//                     System.out.printf("%d встречается %d раз", a[i], count);
+//                 }
+//                 System.out.println();
+//                 count = 0;
+//             }
+//         }
+//     }
+// }
